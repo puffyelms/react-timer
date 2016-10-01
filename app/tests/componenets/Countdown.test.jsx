a584d7faa9b91d604213cfe72ba2e6334e803cc5
Tim Getz <timgetz@gmail.com>
2016-10-01 01:15:35 -0400

    Completed countdown

--- app/tests/componenets/Countdown.test.jsx
+++ app/tests/componenets/Countdown.test.jsx
@@ -37,26 +37,30 @@ describe('Countdown', () => {
                 done();
             },2001);
         })
+
+        it('should pause countdown on paused status', () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+            }, 1001);
+        });
+
+        it('should reset count on stopped', () => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+            }, 1001);
+        });
     });
-    // it('should call onSetCountdown if valid seconds entered', () => {
-    //     var spy = expect.createSpy();
-    //     var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    //     var $el = $(ReactDOM.findDOMNode(countdownForm));
-    //
-    //     countdownForm.refs.seconds.value = '109';
-    //     TestUtils.Simulate.submit($el.find('form')[0]);
-    //     expect(spy).toHaveBeenCalledWith(109);
-    // });
-    //
-    //
-    // it('should not call onSetCountdown if invalid seconds entered', () => {
-    //     var spy = expect.createSpy();
-    //     var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
-    //     var $el = $(ReactDOM.findDOMNode(countdownForm));
-    //
-    //     countdownForm.refs.seconds.value = 'abc';
-    //     TestUtils.Simulate.submit($el.find('form')[0]);
-    //     expect(spy).toNotHaveBeenCalled();
-    // });
 
 });
